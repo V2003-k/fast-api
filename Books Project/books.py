213fecc8eb432605810e550d1e9c502d1fd185cc
V2003-k <vishwajeetkhaple@gmail.com>
2026-03-26 22:17:26 +0530

    [sync]

---
+++ Books Project/books.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "Science"},
+    {"title": "Title Two", "author": "Author Two", "category": "Science"},
+    {"title": "Title Three", "author": "Author Three", "category": "History"},
+    {"title": "Title Four", "author": "Author Four", "category": "Math"},
+    {"title": "Title Five", "author": "Author Five", "category": "Math"},
+]
+
+app = FastAPI()
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
